8fe18860d855b8547ecbcd8f924dbd6993a39f2c
Web Dev Simplified <webdevsimplified@gmail.com>
2019-05-01 21:20:21 -0500

    Initial Commit

---
+++ public/script.js
@@ -0,0 +1,41 @@
+const searchElement = document.querySelector('[data-city-search]')
+const searchBox = new google.maps.places.SearchBox(searchElement)
+searchBox.addListener('places_changed', () => {
+  const place = searchBox.getPlaces()[0]
+  if (place == null) return
+  const latitude = place.geometry.location.lat()
+  const longitude = place.geometry.location.lng()
+  fetch('/weather', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json'
+    },
+    body: JSON.stringify({
+      latitude: latitude,
+      longitude: longitude
+    })
+  }).then(res => res.json()).then(data => {
+    setWeatherData(data, place.formatted_address)
+  })
+})
+
+const icon = new Skycons({ color: '#222' })
+const locationElement = document.querySelector('[data-location]')
+const statusElement = document.querySelector('[data-status]')
+const temperatureElement = document.querySelector('[data-temperature]')
+const precipitationElement = document.querySelector('[data-precipitation]')
+const windElement = document.querySelector('[data-wind]')
+
+icon.set('icon', 'clear-day')
+icon.play()
+
+function setWeatherData(data, place) {
+  locationElement.textContent = place
+  statusElement.textContent = data.summary
+  temperatureElement.textContent = data.temperature
+  precipitationElement.textContent = `${data.precipProbability * 100}%`
+  windElement.textContent = data.windSpeed
+  icon.set('icon', data.icon)
+  icon.play()
+}
---
+++ server.js
@@ -0,0 +1,22 @@
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
+const DARKSKY_API_KEY = process.env.DARKSKY_API_KEY
+const axios = require('axios')
+const express = require('express')
+const app = express()
+
+app.use(express.json())
+app.use(express.static('public'))
+
+app.post('/weather', (req, res) => {
+  axios({
+    url: `https://api.darksky.net/forecast/${DARKSKY_API_KEY}/${req.body.latitude},${req.body.longitude}?units=auto`,
+    responseType: 'json'
+  }).then(data => res.json(data.data.currently))
+})
+
+app.listen(3000, () => {
+  console.log('Server Started')
+})
